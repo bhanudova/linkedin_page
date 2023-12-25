34cb712a412647751d4ccc38cdce292d06122af2
Bhanu_Dova <130201202+bhanudova@users.noreply.github.com>
2023-12-25 08:45:59 +0530

    build issue fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,29 +26,29 @@ export default function Home() {
       case 'about':
         return (
           <div>
-            <h2 className='text-base font-bold'>OverView</h2>
-            <p>This is some demo content for the About tab.</p>
+            <h2 className='text-base font-bold'>Overview</h2>
+            <p>Code View is dedicated to providing comprehensive solutions that redefine excellence in the [industry/field]. Our commitment to quality and innovation drives us to create products and services that stand out in the market. Founded on the principles of integrity and customer satisfaction, we take pride in our journey towards becoming a leader in [industry/field]. Our team consists of passionate individuals with diverse skills and backgrounds. Together, we foster an environment of collaboration and continuous learning. At Code View, we value our people and believe that their creativity and dedication are the driving forces behind our success. Explore our 'About' page to learn more about our mission, vision, and the values that guide us.</p>
           </div>
         );
       case 'post':
         return (
           <div>
             <h2 className='text-base font-bold'>Latest Post</h2>
-            <p>This is some demo content for the Post tab.</p>
+            <p>Welcome to the 'Latest Post' section of Code View. Here, we share insights, updates, and industry trends that matter to you. Our blog is a hub of information where we explore topics ranging from [specific topics] to [industry-related news]. Stay tuned for engaging content that aims to inform, inspire, and keep you connected with the latest developments in [industry/field]. Whether you're a professional in [industry/field] or simply passionate about staying informed, our blog is designed to cater to your interests. Join us on this knowledge-sharing journey as we delve into the latest advancements, best practices, and thought-provoking discussions in [industry/field].</p>
           </div>
         );
       case 'Jobs':
         return (
           <div>
-            <h2 className='text-base font-bold'>Latest Post</h2>
-            <p>This is some demo content for the Post tab.</p>
+            <h2 className='text-base font-bold'>Latest Job Opportunities</h2>
+            <p>Code View is always on the lookout for talented individuals to join our dynamic team. As a company at the forefront of [industry/field] innovation, we offer exciting career opportunities for those who are passionate about making a difference. Explore our 'Jobs' page to discover the latest openings and take the next step in your career with Code View. We believe in creating a work environment that fosters creativity, collaboration, and continuous learning. At Code View, you'll have the opportunity to work on cutting-edge projects, collaborate with industry experts, and contribute to the success of a forward-thinking company. Browse through our job listings and find the perfect match for your skills and aspirations.</p>
           </div>
         );
       case 'People':
         return (
           <div>
-            <h2 className='text-base font-bold'>Latest Post</h2>
-            <p>This is some demo content for the Post tab.</p>
+            <h2 className='text-base font-bold'>Meet Our Team</h2>
+            <p>At Code View, our team is the heart of our success. Meet the dedicated individuals who bring passion, expertise, and creativity to everything we do. Our diverse team consists of professionals with backgrounds in [various disciplines], united by a common goal of driving innovation and excellence in [industry/field]. Each member of our team plays a crucial role in shaping the success of Code View. From project managers to developers, designers to strategists, we celebrate the unique contributions that make us a strong and collaborative unit. Explore our 'People' page to get to know the faces behind Code View and discover the talent that drives our company forward.</p>
           </div>
         );
       default:
